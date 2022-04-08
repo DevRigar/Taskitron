f7d99ae3c70dcf08478799845c1134ed67be8b62
ArisRigas <rigasaristoklis@gmail.com>
2022-04-08 14:25:33 +0300

    Backend Server config
    Controllers and Handlers.

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,30 +1,41 @@
+const asyncHandler = require ('express-async-handler') //Πακέτο που βοηθάει με κάποιο τρόπο
+                                                       //Τη λειτουργία του Mongoose 
+
+
+
+
 //@desc     Get tasks
 //@route    GET /api/tasks
 //@access   Private
-const getTasks = (req,res) => {
+const getTasks = asyncHandler( async (req,res) => {
     res.status(200).json({message:'Get taskitron'})
-}
+})
 
 //@desc     Set task
 //@route    POST /api/tasks
 //@access   Private
-const setTask = (req,res) => {
+const setTask = asyncHandler(async (req,res) => {
+    if (!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field') //Built-in Express Error Handler
+    }
+
     res.status(200).json({message:'Set tasks'})
-}
+})
 
 //@desc     Update task
 //@route    PUT /api/tasks/:id
 //@access   Private
-const updateTask = (req,res) => {
+const updateTask = asyncHandler(async (req,res) => {
     res.status(200).json({message:`Update task ${req.params.id}`})
-}
+})
 
 //@desc     Delete task
 //@route    DELETE /api/tasks/:id
 //@access   Private
-const deleteTask = (req,res) => {
+const deleteTask = asyncHandler(async (req,res) => {
     res.status(200).json({message:`delete task ${req.params.id}`})
-}
+})
 
 module.exports = {
     getTasks,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-
+const {errorHandler} = require ('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())                         //Middleware για να καταλαβαίνει 
+app.use(express.urlencoded({extended: false}))  //jsons forms κτλ.
+
 app.use('/api/taskitron',require('./routes/taskitronRoutes'))
+
+app.use(errorHandler)                           
 
 app.listen(port,()=>console.log(`server started on port ${port}`))
