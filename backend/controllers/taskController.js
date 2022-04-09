69dc2ab18e40e39cc807145ff346e1d7a0a11d69
ArisRigas <rigasaristoklis@gmail.com>
2022-04-09 15:40:33 +0300

    CRUD functionality added
    CRUD functionality, Task model creation

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require ('express-async-handler') //Πακέτο που βοηθάει με κάποιο τρόπο
                                                        //Τη λειτουργία του Mongoose 
-
+const Task = require('../models/taskModel')
 
 
 
@@ -8,33 +8,59 @@ const asyncHandler = require ('express-async-handler') //Πακέτο που β�
 //@route    GET /api/tasks
 //@access   Private
 const getTasks = asyncHandler( async (req,res) => {
-    res.status(200).json({message:'Get taskitron'})
+    const tasks = await Task.find()
+
+    res.status(200).json(tasks)
 })
 
 //@desc     Set task
 //@route    POST /api/tasks
 //@access   Private
 const setTask = asyncHandler(async (req,res) => {
-    if (!req.body.text){
+    if (!req.body.title){
         res.status(400)
         throw new Error('Please add a text field') //Built-in Express Error Handler
     }
 
-    res.status(200).json({message:'Set tasks'})
+    const task = await Task.create({
+        title:req.body.title,
+    })
+
+    res.status(200).json(task)
 })
 
 //@desc     Update task
 //@route    PUT /api/tasks/:id
 //@access   Private
 const updateTask = asyncHandler(async (req,res) => {
-    res.status(200).json({message:`Update task ${req.params.id}`})
+    const task = await Task.findById(req.params.id)
+
+    if(!task){
+        res.status(400)
+        throw new Error('Task not found')        
+    }
+
+    const updatedTask= await Task.findByIdAndUpdate(req.params.id, req.body,{
+        new:true,
+    })
+
+    res.status(200).json(updatedTask)
 })
 
 //@desc     Delete task
 //@route    DELETE /api/tasks/:id
 //@access   Private
 const deleteTask = asyncHandler(async (req,res) => {
-    res.status(200).json({message:`delete task ${req.params.id}`})
+    const task = await Task.findById(req.params.id)
+
+    if(!task){
+        res.status(400)
+        throw new Error('Task not found')        
+    }
+
+    await task.remove()
+
+    res.status(200).json({id:req.params.id})
 })
 
 module.exports = {
